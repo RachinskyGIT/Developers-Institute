c0879b1756469fb93dd26a2264f801a62b1ce0f3
RachinskyGIT <rachinsky1995@gmail.com>
2023-03-29 14:32:33 +0300

    update w2d5

--- Week-2/Day-5/Day-5.py
+++ Week-2/Day-5/Day-5.py
@@ -1,58 +1,28 @@
+######################### Beginning of the game code ######################### 
+######################### The game code is completely written by RachinskyGIT ######################### 
+#########################           https://github.com/RachinskyGIT           ######################### 
+
 import os
 import time
-def clearscr():
-    os.system('cls')
-
-
-# fielddict = {1:".",2:".",3:".",4:".",5:".",6:".",7:".",8:".",9:"."}
-
-
-
-# # def wh(): 
-# #     choice = input("Choice area: ")
-# #     return(choice)
-# choice = 5
-# choice = int(choice)
-
-# turn=False  #True="X", False="O"
-
-
-
-# while choice in range(1,10):
-#     if turn==True:
-#         fielddict[choice]="X"
-#     else:
-#         fielddict[choice]="O"
-#     break
-
-# n1,n2,n3,n4,n5,n6,n7,n8,n9 = [list(fielddict.items())[0][1],list(fielddict.items())[1][1]\
-#                               ,list(fielddict.items())[2][1],list(fielddict.items())[3][1]\
-#                                 ,list(fielddict.items())[4][1],list(fielddict.items())[5][1]\
-#                                     ,list(fielddict.items())[6][1],list(fielddict.items())[7][1]\
-#                                         ,list(fielddict.items())[8][1]]
-
-
-
-
-
-# print(line)
 
 def game(turn, gamedict):
-    os.system('cls')
+    os.system('cls') #clear the screen every turn
     gameend=False
     if gamedict=="start":
-        turn=True #"X" starts
+        turn=True #"X"-player start the game (if False -> "O"-player start)
         fielddict = {"1":".","2":".","3":".","4":".","5":".","6":".","7":".","8":".","9":"."}
     else:
         fielddict=gamedict
 
+    #n's transfer cell value from dictionary to grid
     n1,n2,n3,n4,n5,n6,n7,n8,n9 = [list(fielddict.items())[0][1],list(fielddict.items())[1][1]\
                               ,list(fielddict.items())[2][1],list(fielddict.items())[3][1]\
                                 ,list(fielddict.items())[4][1],list(fielddict.items())[5][1]\
                                     ,list(fielddict.items())[6][1],list(fielddict.items())[7][1]\
                                         ,list(fielddict.items())[8][1]]
     
-    line = f"\
+    #The Grid
+    line = f'\n\n\n\
     \u269E \u2655 \u2655  \u2655 \u2654 \u2655 \u2655 \u2655 \u269F\n\
     |1    |2    |3    |\n\
     |  {n1}  |  {n2}  |  {n3}  |\n\
@@ -62,37 +32,44 @@ def game(turn, gamedict):
     |_____|_____|_____|\n\
     |7    |8    |9    |\n\
     |  {n7}  |  {n8}  |  {n9}  |\n\
-    |_____|_____|_____|\n"
+    |_____|_____|_____|\n    "stop" for endgame\n'
 
     print(line)
+    # print('Choose "stop" to interrupt the game')
+
     if turn==True:
         print('Player "X" turn\n')
     else:
         print('Player "O" turn\n')
 
-
-
+    #Victory condition
     if (n1==n2==n3!=".") or (n4==n5==n6!=".") or (n7==n8==n9!=".") or\
        (n1==n4==n7!=".") or (n2==n5==n8!=".") or (n3==n6==n9!=".") or\
        (n1==n5==n9!=".") or (n3==n5==n7!="."):
-        gameend=True
+        gameend=True 
 
-    if gameend==True:
-        print("The Game is End")
-        time.sleep(0.5)
-        if turn==True:
-            print('The "O"\u2015player is a winner! Congratulations!')
-        else:
-            print('The "X"\u2015player is a winner! Congratulations!')
-        time.sleep(1.5)
+    #Draw condition and action
+    if (n1!=".") and (n2!=".") and (n3!=".") and (n4!=".") and (n5!=".") \
+        and (n6!=".") and (n7!=".") and (n8!=".") and (n9!=".") and (gameend!=True):
+        print("The Game ended in a Draw -_-'\nGame will close in 5 seconds.")
+        time.sleep(5)
         quit()
 
-
+    #Victory action  
+    if gameend==True:
+        print("End of the Game")
+        time.sleep(0.5)
+        if turn==True:
+            print('The "O"-player is a winner! Congratulations ^_^')
+        else:
+            print('The "X"-player is a winner! Congratulations ^_^')
+        time.sleep(1.5)
+        quit()
 
     goodchoice = ["1","2","3","4","5","6","7","8","9"]
     flag=True
     while flag==True:
-        choice = input("Choice area: ")
+        choice = input("Choice area (1-9): ")
         if choice == "stop":
             print("You choosed to end the game.\nGame will close in 5 seconds.")
             time.sleep(5)
@@ -112,7 +89,11 @@ def game(turn, gamedict):
             turn = not turn
             break
     game(turn, fielddict)
-    
+######################### End of game code ######################### 
+######################### The game code is completely written by RachinskyGIT ######################### 
+#########################           https://github.com/RachinskyGIT           ######################### 
+
+
         
 game(0,"start")
 
