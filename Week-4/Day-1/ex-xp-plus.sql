606746683d4c2e13cbed72bef41849dc39cd6cc1
RachinskyGIT <rachinsky1995@gmail.com>
2023-04-17 23:30:54 +0300

    updated w4d1

---
+++ Week-4/Day-1/ex-xp-plus.sql
@@ -0,0 +1,31 @@
+CREATE TABLE students (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  birth_date DATE
+);
+
+INSERT INTO students (first_name, last_name, birth_date)
+VALUES
+  ('Marc', 'Benichou', '1998-11-02'),
+  ('Yoan', 'Cohen', '2010-12-03'),
+  ('Lea', 'Benichou', '1987-07-27'),
+  ('Amelia', 'Dux', '1996-04-07'),
+  ('David', 'Grez', '2003-06-14'),
+  ('Omer', 'Simpson', '1980-10-03');
+  
+
+SELECT first_name, last_name FROM public.students ;
+SELECT first_name, last_name FROM public.students where id=2 ;
+SELECT first_name, last_name FROM public.students where  first_name LIKE 'A%' ;
+
+SELECT * FROM students
+WHERE first_name LIKE '%a%' AND
+      first_name NOT LIKE 'a%' AND
+      first_name NOT LIKE '%a' AND
+      SUBSTRING(first_name, 2, LENGTH(first_name)-2) LIKE '%a%';
+
+
+SELECT * FROM students
+WHERE birth_date >= '2000-01-01'
+
