3901f70fe9955490b33f113bfb80decab2f35a54
RachinskyGIT <rachinsky1995@gmail.com>
2023-05-28 20:52:27 +0300

    not mandatory exercise

---
+++ Week-9/Day-1/second_daily.js
@@ -0,0 +1,9 @@
+function allTruthy(...values) {
+    return values.every((value) => Boolean(value));
+  }
+  
+
+  console.log(allTruthy(true, true, true)); // Output: true
+  console.log(allTruthy(true, false, true)); // Output: false
+  console.log(allTruthy(5, 4, 3, 2, 1, 0)); // Output: false
+  
