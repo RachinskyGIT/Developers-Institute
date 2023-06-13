57e6c2ad5739c07794fd4ab6106532716bf1240a
RachinskyGIT <rachinsky1995@gmail.com>
2023-06-14 00:27:15 +0300

    added w11d3

---
+++ Week-11/Day-3/ErrorBoundary.js
@@ -0,0 +1,31 @@
+// import React from 'react';
+
+// class ErroeBoundary extends React.Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             hasError: false;
+//             error:null,
+//             errorInfo: null,
+//         }
+//     }
+
+
+//     componentDidCatch(error, errorInfo) {
+//         console.log('error=>', error);
+//         console.log('errorInfo=>', errorInfo);
+//         this.setState({hasError:true, error:error, errorInfo:errorInfo})
+//     }
+
+//     render() {
+//         if (this.state.hasError){
+//             return (
+//                 <div>
+//                 'Something went wrong with error'
+//                 </div>
+//             )
+//         }
+//     }
+//     return this.props.children
+// }
+// export default ErrorBoudary
